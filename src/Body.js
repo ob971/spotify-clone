31b609e708addfaf252f9e773dcf7d33a08c133c
obsa <obsukebede2022@gmail.com>
2023-05-01 12:00:00 +0300

    upating body error

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Body.css";
-// // import Header from "./Header";
-// // import { useStateValue } from "./StateProvider";
-// // import SongRow from "./SongRow";
-// // import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
-// // import FavoriteIcon from "@material-ui/icons/Favorite";
-// import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+ import Header from "./Header";
+ import { useStateValue } from "./StateProvider";
+  import SongRow from "./SongRow";
+ import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+ import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useStateValue();
