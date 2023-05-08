1858983d173fa14e69669dbe7722749a5c40a920
obsa <obsukebede2022@gmail.com>
2023-05-08 12:00:00 +0300

    upadting

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ function Login() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <a href={accessUrl}>LOGIN TO SPOTIFY</a>
+      <a href={accessUrl}>LOGIN TO SPOTI FY</a>
     </div>
   );
 }
